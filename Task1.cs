fb19b9c3ca46329e70bba25559dd53426b34c4e0
Denis280303 <kozlovskyidenus@gmail.com>
2022-07-01 23:00:57 +0300

    TEST
    TEST

---
+++ Task1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+namespace task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string text = Console.ReadLine();
+            Console.Write("Enter num: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(encrypt(text,n));
+            //Console.WriteLine(decrypt(text, n));
+        }
+        static string encrypt(string text, int n)
+        {
+            if (string.IsNullOrEmpty(text) || n <= 0)
+            {
+                return text;
+            }
+            string encrypted_text = "";
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 1; i < text.Length; i += 2)
+                {
+                    encrypted_text += text[i];
+                }
+                for (int i = 0; i < text.Length; i += 2)
+                {
+                    encrypted_text += text[i];
+                }
+                text = encrypted_text;
+                encrypted_text = "";
+            }
+            return text;
+        }
+//----------Variant 2 (encrypt) -----------
+        //static string encrypt(string text, int n)
+        //{
+        //    if (string.IsNullOrEmpty(text) || n <= 0)
+        //    {
+        //        return text;
+        //    }
+        //    string pair = "";
+        //    string npair = "";
+        //    for (int j = 0; j < n; j++)
+        //    {
+        //        for (int i = 1; i < text.Length; i += 2)
+        //        {
+        //            pair += text[i];
+        //        }
+        //        for (int i = 0; i < text.Length; i += 2)
+        //        {
+        //            npair += text[i];
+        //        }
+        //        text = String.Concat(pair, npair);
+        //        pair = "";
+        //        npair = "";
+
+        //    }
+        //    return text;
+        //}
+        static string decrypt(string encrypted_text, int n)
+        {
+            if (string.IsNullOrEmpty(encrypted_text) || n <= 0)
+            {
+                return encrypted_text;
+            }
+            string text = "";
+            for (int j = 0; j < n; j++)
+            {
+                int av = (encrypted_text.Length) / 2;
+                int p = 0;
+                for (int i = av; i <encrypted_text.Length; i++)
+                {
+                    text += encrypted_text[i];
+                    if (p< (encrypted_text.Length / 2))
+                    {
+                        text += encrypted_text[p];
+                        p++;
+                    }
+                }
+                encrypted_text = text;
+                text = "";
+            }return encrypted_text;
+        }
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace task2
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string text = Console.ReadLine();
+            task2(text);
+        }
+
+        static void task2(string s)
+        {
+            IDictionary<string, int> numWord = new Dictionary<string, int>();
+            Regex regex = new Regex(@"[a-zA-Z][a-zA-Z']*");
+            MatchCollection formatMatch = regex.Matches(s);
+            foreach (Match match in formatMatch)
+            {
+                try
+                {
+                    numWord.Add(match.Value, 1);
+                }
+                catch
+                {
+                    numWord[match.Value] += 1;
+                }
+            }
+            string[] arr = new string[numWord.Count];
+            numWord.Keys.CopyTo(arr, 0);
+
+            if (numWord.Count < 3)
+            {
+                Array.Clear(arr, 0, arr.Length);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(numWord.OrderByDescending(key => key.Value).ElementAt(i).Key.ToLower());
+                }
+            }
+        }
+    }
+}
